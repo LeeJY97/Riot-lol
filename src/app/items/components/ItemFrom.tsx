950c843608d9c7b0de60dfb1df7aafcf3f054442
LeeJY97 <ijoonyeol97@gmail.com>
2024-10-02 00:11:41 +0900

    [feat] 아이템 조합식 (하위 아이템 트리 차트)

--- src/app/items/components/ItemFrom.tsx
+++ src/app/items/components/ItemFrom.tsx
@@ -2,6 +2,7 @@ import { convertArrayToItemTable } from "@/service/itemService";
 import { Item, ItemCustomExtend, ItemTable } from "@/types/Item";
 import Image from "next/image";
 import React from "react";
+import "./tree.scss";
 
 type Props = {
   items: (Item & ItemCustomExtend)[];
@@ -12,88 +13,71 @@ type NestedItemFromTree = {
   [key: string]: NestedItemFromTree | null;
 };
 
-const ItemTree = ({ tree }: NestedItemFromTree) => {
-  return (
-    <>
-      <div className="tree">
-        <ul>
-          <li>
-            <Image
-              width={50}
-              height={50}
-              src="https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/1001.png"
-              alt=""
-            />
-            {/* <a href="#">Parent</a> */}
-            <ul>
-              <li>
-                <Image
-                  width={50}
-                  height={50}
-                  src="https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/1001.png"
-                  alt=""
-                />
-                <ul>
-                  <li>
-                    <Image
-                      width={50}
-                      height={50}
-                      src="https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/1001.png"
-                      alt=""
-                    />
-                  </li>
-                  <li>
-                    <Image
-                      width={50}
-                      height={50}
-                      src="https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/1001.png"
-                      alt=""
-                    />
-                  </li>
-                  <li>
-                    <Image
-                      width={50}
-                      height={50}
-                      src="https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/1001.png"
-                      alt=""
-                    />
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <Image
-                  width={50}
-                  height={50}
-                  src="https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/1001.png"
-                  alt=""
-                />
-                <ul>
-                  <li>
-                    <Image
-                      width={50}
-                      height={50}
-                      src="https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/1001.png"
-                      alt=""
-                    />
-                  </li>
-                  <li>
-                    <Image
-                      width={50}
-                      height={50}
-                      src="https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/1001.png"
-                      alt=""
-                    />
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+type TreeProps = {
+  tree: NestedItemFromTree;
+  itemTable: ItemTable;
 };
 
+const ItemTree = ({ tree, itemTable }: TreeProps) => {
+  const renderTree = () => {
+    const result = [];
+
+    // 최상위 아이템 순회
+    for (const parentId in tree) {
+      const parentItem = itemTable[parentId];
+      const parentLi = (
+        <li key={parentId}>
+          <Image
+            width={50}
+            height={50}
+            src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/item/${parentId}.png`}
+            alt={parentItem ? parentItem.name : `아이템 ${parentId} 없음`}
+          />
+          {/* 하위 아이템이 있을 경우 */}
+          {tree[parentId] && Object.keys(tree[parentId]).length > 0 && (
+            <ul>{renderChildren(tree[parentId])}</ul>
+          )}
+        </li>
+      );
+      result.push(parentLi);
+    }
+
+    return result;
+  };
+  const renderChildren = (children: NestedItemFromTree) => {
+    const childItems = [];
+
+    // 하위 아이템 순회
+    for (const childId in children) {
+      const childItem = itemTable[childId];
+      const childLi = (
+        <li key={childId}>
+          <Image
+            width={50}
+            height={50}
+            src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/item/${childId}.png`}
+            alt={childItem ? childItem.name : `아이템 ${childId} 없음`}
+          />
+          {/* 더 하위 아이템이 있을 경우 */}
+          {children[childId] && Object.keys(children[childId]).length > 0 && (
+            <ul>{renderChildren(children[childId])}</ul>
+          )}
+        </li>
+      );
+      childItems.push(childLi);
+    }
+
+    return childItems;
+  };
+
+  return (
+    <div className="tree">
+      <ul>
+        {renderTree()} {/* 최상위 아이템 렌더링 */}
+      </ul>
+    </div>
+  );
+};
 const ItemFrom = ({ items, selectedItem }: Props) => {
   if (!selectedItem.from) {
     return <div>하위 아이템이 없습니다.</div>;
@@ -101,10 +85,9 @@ const ItemFrom = ({ items, selectedItem }: Props) => {
   const itemTable = convertArrayToItemTable(items);
 
   const itemFromTree = buildItemHierarchy(selectedItem.id, itemTable);
-  console.log("tree", itemFromTree);
   return (
     <div className="">
-      <ItemTree tree={{ [selectedItem.id]: itemFromTree }} />
+      <ItemTree tree={{ [selectedItem.id]: itemFromTree }} itemTable={itemTable} />
     </div>
   );
 };
