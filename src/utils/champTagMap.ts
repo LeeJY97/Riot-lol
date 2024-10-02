2cdaf3f60374840e7ab851194fb9785bec6a53a5
LeeJY97 <ijoonyeol97@gmail.com>
2024-10-03 03:39:49 +0900

    [refactor] 챔피언 태그 enum으로 변경

--- src/utils/champTagMap.ts
+++ src/utils/champTagMap.ts
@@ -1,9 +1,19 @@
-const tagMap = {
-  Fighter: "전사",
-  Tank: "탱커",
-  Assassin: "암살자",
-  Mage: "마법사",
-  Marksman: "원거리딜러",
-  Support: "서포터",
+export enum CHAMP_TAGS {
+  Fighter = "Fighter",
+  Tank = "Tank",
+  Assassin = "Assassin",
+  Mage = "Mage",
+  Marksman = "Marksman",
+  Support = "Support",
+}
+
+export type Tags = keyof typeof CHAMP_TAGS;
+
+export const champTagMapKr = {
+  [CHAMP_TAGS.Fighter]: "전사",
+  [CHAMP_TAGS.Tank]: "탱커",
+  [CHAMP_TAGS.Assassin]: "암살자",
+  [CHAMP_TAGS.Mage]: "마법사",
+  [CHAMP_TAGS.Marksman]: "원거리딜러",
+  [CHAMP_TAGS.Support]: "서포터",
 };
-export default tagMap;
