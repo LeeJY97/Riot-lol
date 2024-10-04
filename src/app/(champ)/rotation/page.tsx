27f9fed52f4c0c2bbcdd79497b2007cb31ce62f4
LeeJY97 <ijoonyeol97@gmail.com>
2024-10-04 14:17:15 +0900

    [test] 버셀 useQuery

--- src/app/(champ)/rotation/page.tsx
+++ src/app/(champ)/rotation/page.tsx
@@ -5,34 +5,38 @@ import { getChamps } from "@/server-actions/champAction";
 import { getChampsExtendCustomImage, getChampsWithRotations } from "@/service/champService";
 import { ChampTable } from "@/types/Champs";
 import type Rotation from "@/types/Rotation";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import ChampGrid from "../components/ChampGrid";
 
 const Rotation = () => {
-  // console.log(`Fetching from: ${process.env.NEXT_PUBLIC_BASE_URL}/api/rotation`);
-  // const { data: rotationKeys } = useSuspenseQuery<Rotation>({
-  //   queryKey: queryKey.rotation.rotationKeys,
-  //   queryFn: async () => {
-  //     const rotationRes = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/rotation`);
-  //     // const rotationRes = await fetch(`/api/rotation`);
-  //     const rotationKeys = await rotationRes.json();
-  //     return rotationKeys;
-  //   },
-  // });
+  console.log(`Fetching from: ${process.env.NEXT_PUBLIC_BASE_URL}/api/rotation`);
+  const { data: rotationKeys, isPending: rotationPending } = useQuery<Rotation>({
+    queryKey: queryKey.rotation.rotationKeys,
+    queryFn: async () => {
+      const rotationRes = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/rotation`);
+      // const rotationRes = await fetch(`/api/rotation`);
+      const rotationKeys = await rotationRes.json();
+      return rotationKeys;
+    },
+  });
 
-  // const { data: ChampTable } = useSuspenseQuery<ChampTable>({
-  //   queryKey: queryKey.champ.champs,
-  //   queryFn: () => getChamps(),
-  //   initialData: {},
-  //   staleTime: 0,
-  // });
+  const { data: ChampTable, isPending: champTablePending } = useQuery<ChampTable>({
+    queryKey: queryKey.champ.champs,
+    queryFn: () => getChamps(),
+    initialData: {},
+    staleTime: 0,
+  });
 
-  // const rotationChamps = getChampsWithRotations(rotationKeys, ChampTable);
-  // const champsExtendCustomImage = getChampsExtendCustomImage(rotationChamps);
+  if (champTablePending || rotationPending) {
+    return <></>;
+  }
+
+  const rotationChamps = getChampsWithRotations(rotationKeys, ChampTable);
+  const champsExtendCustomImage = getChampsExtendCustomImage(rotationChamps);
 
   return (
     <div className="max-w-[1200px]  min-w-[990px] p-4">
-      {/* <ChampGrid champs={champsExtendCustomImage} />; */}
+      <ChampGrid champs={champsExtendCustomImage} />;
     </div>
   );
 };
